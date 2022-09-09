6844c25a93953d92d8305d75502001506163d76f
Daniel Sobit <sobitdaniel@gmail.com>
2022-09-09 07:04:49 +0300

    init ActiveRecord

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,20 +1,2 @@
-class User
-  attr_accessor :name, :email, :id
-
-  def initialize(name:, email:, id: nil)
-    @id = id
-    @name = name
-    @email = email
-  end
-
-  def self.create_table
-    sql = <<-SQL
-        CREATE TABLE IF NOT EXISTS users(
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            email TEXT
-        )
-    SQL
-    DB[:conn].execute(sql)
-  end
+class User < ActiveRecord::Base
 end
